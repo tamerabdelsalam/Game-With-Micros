37f88dc1b01b0e477b8594cb6114eee457473a57
tamer.abdelsalam <tamer.abdelsalam@aspiresoftware.com>
2023-02-15 18:31:50 +0200

    Prepared initial project files and .gitignore

--- MicroServices/Play.Catalog/src/Play.Catalog.Service/Controllers/WeatherForecastController.cs
+++ MicroServices/Play.Catalog/src/Play.Catalog.Service/Controllers/WeatherForecastController.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Play.Catalog.Servcice;
 
-namespace Play.Catalog.Servcice.Controllers;
+namespace Play.Catalog.Service.Controllers;
 
 [ApiController]
 [Route("[controller]")]
