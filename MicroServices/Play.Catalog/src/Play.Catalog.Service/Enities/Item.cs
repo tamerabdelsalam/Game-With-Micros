e07fbfbfc670f40164be6ed8007ec36088ede3fe
tamer.abdelsalam <tamer.abdelsalam@aspiresoftware.com>
2023-03-06 15:18:36 +0200

    Implemented first mongo db repository

---
+++ MicroServices/Play.Catalog/src/Play.Catalog.Service/Enities/Item.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service.Enities;
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description{ get; set; }    
+    public decimal Price{ get; set; }    
+}
