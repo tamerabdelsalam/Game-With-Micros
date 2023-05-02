25fcda969322fbed88b0176dbc5b46eb8155f2e5
tamer.abdelsalam <tamer.abdelsalam@aspiresoftware.com>
2023-05-02 17:22:41 +0300

    Added Pack. Play.Catalog.Contracts and upgraded Play.Common Pack.

---
+++ MicroServices/Play.Inventory/src/Play.Inventory.Service/Entities/CatalogItem.cs
@@ -0,0 +1,11 @@
+using System;
+using Play.Common.Interfaces;
+
+namespace Play.Inventory.Service.Entities;
+
+public class CatalogItem : IEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
